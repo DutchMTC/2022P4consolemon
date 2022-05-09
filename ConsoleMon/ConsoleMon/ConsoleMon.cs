c7a5375994662e3c5e92e88568d5280cdbb7753d
Miyaki Truong <32574@ma-web.nl>
2022-05-09 09:56:43 +0200

    Completed first 5 assignments

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+
+        internal void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
+
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy = this.energy - energy;
+        }
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        internal int damage;
+        internal int energyCost;
+        internal string name;
+
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+    }
+}
